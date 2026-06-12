a8c6f5588ca5317af64aaed351007f4832f39a45
Nathan Gueroult <nathan.gueroult.34@gmail.com>
2026-06-13 00:35:38 +0200

    [UPDATE] auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,48 @@
-import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import express, { Router } from 'express';
+const router = Router();
 const db = require('better-sqlite3')('../create_db.db');
-const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    const { email, password } = req.body;
+    const { email, username, password } = req.body;
 
     try {
         if (!email || !password) {
-            return res.status(400).json({ message: 'Tous les champs sont necessaires'});
+            return res.status(400).json({ message: 'All fields are required'});
         }
+
+        const userExists = db.prepare(`SELECT * FROM Users WHERE email = ?`).get(`email`);
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists'});
+        }
+        
+        const newUser = db.prepare(`INSERT INTO Users VALUES (?, ?, ?)`).run(email, username, password);
+        res.status(201).json({ message: 'User registered successfully', user: newUser });
+    } catch (err) {
+        res.status(500).json({ message: 'Server error' });
     }
-})
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = db.prepare(`SELECT * FROM Users WHERE email = ?`).get(`email`);
+        if (!userExists) {
+            return res.status(400).json({ message: 'Invalid credencials' });
+        }
+
+        const verifyPassword = bcrypt.compare(password, user[password]);
+        if (!verifyPassword) {
+            return res.status(400).json({ message: 'Invalid credencials' });
+        }
+
+        const token = jwt.sign({ id: user[id] }, process.env.JWT_KEY, { expiresIn: '2h'});
+        res.json({ message: 'Login successfully', token });
+    } catch (err) {
+        res.status('500').json({ message: 'Server error' });
+    }
+});
+
+module.exports = router;
