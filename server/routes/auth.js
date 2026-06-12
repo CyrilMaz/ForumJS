357977a4a1736595da430cbb03adeeee66d934d2
Nathan Gueroult <nathan.gueroult.34@gmail.com>
2026-06-12 10:03:25 +0200

    [FIX] auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,15 @@
-const express = require('express');
-const bcrypt = require('bcrypt')
+import express,  from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+const db = require('better-sqlite3')('../create_db.db');
+const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Tous les champs sont necessaires'});
+        }
+    }
 })
