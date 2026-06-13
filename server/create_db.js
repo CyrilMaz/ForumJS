828d4af1ba5788ece1ad40675d9e26273374b9e5
cyril <cyril.mazauric@ynov.com>
2026-06-13 11:08:24 +0200

    [FIX] Syntax

--- server/create_db.js
+++ server/create_db.js
@@ -4,57 +4,56 @@ const db = new Database('./data/database.db');
 
 function startdb() {
     db.exec(`
-        CREATE IF NOT EXISTS TABLE Users (
+        CREATE TABLE IF NOT EXISTS Users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             email NVARCHAR(255) NOT NULL UNIQUE,
             username VARCHAR(100) NOT NULL,
             password TEXT NOT NULL
-        )
+        );
 
-        CREATE IF NOT EXISTS TABLE Categories (
+        CREATE TABLE IF NOT EXISTS Categories (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR(100) NOT NULL
-        )
+        );
 
-        CREATE IF NOT EXISTS TABLE Posts (
+        CREATE TABLE IF NOT EXISTS Posts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
             category_id INTEGER NOT NULL,
             title VARCHAR(100) NOT NULL,
             content TEXT NOT NULL,
             date TEXT DEFAULT datetime('now') NOT NULL,
-            FOREIGN KEY(user_id) REFERENCES Users(id)
+            FOREIGN KEY(user_id) REFERENCES Users(id),
             FOREIGN KEY(category_id) REFERENCES Categories(id)
-        )
+        );
 
-        CREATE IF NOT EXISTS TABLE Post_likes (
+        CREATE TABLE IF NOT EXISTS Post_likes (
             user_id INTEGER NOT NULL,
             post_id INTEGER NOT NULL,
             value INTEGER NOT NULL,                         -- 1=like, 0=dislike,
             PRIMARY KEY(user_id, post_id),
             FOREIGN KEY(user_id) REFERENCES Users(id),
-            FOREIGN KEY(post_id) REFERENCES Posts(id),
-        )
+            FOREIGN KEY(post_id) REFERENCES Posts(id)
+        );
 
-        CREATE IF NOT EXISTS TABLE Comments (
+        CREATE TABLE IF NOT EXISTS Comments (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
             post_id INTEGER NOT NULL,
             content TEXT NOT NULL,
             date TEXT DEFAULT datetime('now') NOT NULL,
-            PRIMARY KEY(user_id, post_id),
             FOREIGN KEY(user_id) REFERENCES Users(id),
             FOREIGN KEY(post_id) REFERENCES Posts(id)
-        )
+        );
 
-        CREATE IF NOT EXISTS TABLE Comments_likes (
+        CREATE TABLE IF NOT EXISTS Comments_likes (
             user_id INTEGER NOT NULL,
             comment_id INTEGER NOT NULL,
             value INTEGER NOT NULL,                         -- 1=like, 0=dislike
             PRIMARY KEY(user_id, comment_id),
             FOREIGN KEY(user_id) REFERENCES Users(id),
             FOREIGN KEY(comment_id) REFERENCES Comments(id)
-        )
+        );
     `);
  }
 
