030d3834db9360a161d12314f006f37f1b516030
cyril <cyril.mazauric@ynov.com>
2026-06-13 11:51:43 +0200

    [FIX] added authRouter and app.use

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import postsRouter from './routes/posts.js';
+import authRouter from './routes/auth.js';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());          // autorise React (5173) à appeler Express (3000)
 app.use(express.json());  // lit le body JSON des requêtes POST/PUT
 
 app.use('/api/posts', postsRouter);
+app.use('/api/auth', authRouter)
 
 app.listen(3000, () => console.log('Serveur sur http://localhost:3000'));
