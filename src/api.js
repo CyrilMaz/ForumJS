6a1294c13e388269578a3dad8703a92f60b78a16
Cyril Mazauric <cyril.mazauric@orange.fr>
2026-06-16 02:19:30 +0200

    [FIX] rewrite all api.js function to fetch

--- src/api.js
+++ src/api.js
@@ -2,13 +2,28 @@
 const API_URL = "/api"
 
 export async function fetchPosts() {
-  return fetch(`${API_URL}/posts`).then(r => r.json())
+  return apiFetch('/posts');
 }
 
-export async function createPost(form) {
-  return fetch(`${API_URL}/posts`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(form) }).then(r => r.json())}
 
-export async function votePost(postId, field) {
-  return fetch(`${API_URL}/posts/${postId}/vote`, { method: 'PATCH', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ field }) }).then(r => r.json())
-  
+export async function createPost(form, token) {
+  return apiFetch('/posts', { method: 'POST', body: JSON.stringify(form) }, token);
+}
+
+
+export async function votePost(postId, value, token) {
+  return apiFetch(`/posts/${postId}/vote`, { method: 'PATCH', body: JSON.stringify({ value }) }, token);
+}
+
+export async function createComment(postId, content, token) {
+  return apiFetch(`/posts/${postId}/comments`, { method: 'POST', body: JSON.stringify({ content }) }, token);
+}
+
+export async function apiFetch(path, options ={}, token) {
+  const headers = { 'Content-Type': 'application/json' };
+  if (token) headers['Authorization'] = `Bearer ${token}`;
+  const ress = await fetch(`/api${path}`, { ...options, headers });
+  const data = await ress.json();
+  if (!ress.ok) throw new Error(data.message || 'Erreur serveur');
+  return data
 }
